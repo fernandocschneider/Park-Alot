9b8bdac846a23007485ed76c7960848bdae6af5d
JeanToral <jean.toral@unoesc.edu.br>
2024-11-05 21:07:07 -0300

    Changed pages path

--- Backend/parkalot/parkalot/src/main/java/br/edu/unoesc/parkalot/controllers/GreetingsController.java
+++ Backend/parkalot/parkalot/src/main/java/br/edu/unoesc/parkalot/controllers/GreetingsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,33 +19,13 @@ import br.edu.unoesc.parkalot.model.Cliente;
 
 @RestController
 public class GreetingsController {
-    // @GetMapping("/")
-    // public String index() {
-    //     return "Hello, World!";
-    // }
-
-    // @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    // @ResponseStatus(HttpStatus.OK)
-    // public String texto(@PathVariable String name) {
-    //     return "Hello, " + name + " !";
-    // }
-
     @Autowired
     private br.edu.unoesc.parkalot.repository.clienteRepository clienteRepository;
-
-    // @RequestMapping(value = "testeGravar/{nome}", method = RequestMethod.GET)
-    // @ResponseStatus(HttpStatus.OK)
-    // public String testeGravar(@PathVariable String nome) {
-    //     Cliente cliente = new Cliente();
-    //     cliente.setName(nome);
-    //     cliente.setCpf("12345678901");
-    //     cliente.setEmail("teste@teste.com");
-    //     cliente.setPhone("1234567890");
-    //     cliente.setSex("M");
-    //     cliente.setAge(12);
-    //     clienteRepository.save(cliente);
-    //     return "Gravado";
-    // }
+    
+    @GetMapping("/home")
+    public String home() {
+        return "index";
+    }
 
     @GetMapping(value = "listatodos")
     @ResponseBody
