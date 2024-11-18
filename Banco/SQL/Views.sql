6d400032e7122f234e36f21bb5204b3091545319
JeanToral <128260288+JeanToral@users.noreply.github.com>
2024-11-18 10:48:52 -0300

    Update views

--- Banco/SQL/Views.sql
+++ Banco/SQL/Views.sql
@@ -1,23 +1,23 @@
 CREATE VIEW Relatorio_Clientes_Veiculos AS
-SELECT c.clie_nome, c.clie_tele, c.clie_email, v.veic_placa, v.veic_marca, v.veic_model, v.veic_ano
+SELECT c.clint_name, c.clint_phone, c.clint_mail, v.veicule_plate, v.veicule_brand, v.veicule_model, v.veicule_year
 FROM Cliente c
-JOIN Veiculo v ON c.clie_cpf = v.clie_cpf;
+JOIN Veiculo v ON c.clint_cpf = v.clint_cpf;
 
 SELECT * FROM Relatorio_Clientes_Veiculos;
 
 
 CREATE VIEW Relatorio_Manutencao_Funcionario AS
-SELECT f.func_nome, f.func_cargo, m.manut_desc, m.manut_data, m.manut_custo
+SELECT f.worker_name, f.worker_role, m.fix_description, m.fix_date, m.fix_cost
 FROM Manutencao m
-JOIN Funcionario f ON m.func_cpf = f.func_cpf;
+JOIN Funcionario f ON m.worker_role = f.worker_cpf;
 
 SELECT * FROM Relatorio_Manutencao_Funcionario;
 
 
 CREATE VIEW Relatorio_Clientes_Enderecos AS
-SELECT c.clie_nome, e.ender_rua, e.ender_bairro, e.ender_comp, e.ender_cida
+SELECT c.client_name, e.adress_street, e.adress_neighbor, e.adress_extra, e.adress_city
 FROM Cliente c
-JOIN Endereco e ON c.clie_cpf = e.clie_cpf;
+JOIN Endereco e ON c.client_cpf = e.client_cpf;
 
 SELECT * FROM Relatorio_Clientes_Enderecos;
 
